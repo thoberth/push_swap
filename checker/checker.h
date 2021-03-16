cc15bd3f369fe06f8d82c497a297f2d7ccb36a98
Thomas BERTHET <thoberth@e2r9p20.42.fr>
2021-03-16 16:13:06 +0100

    WIP

---
+++ checker/checker.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thoberth <thoberth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/16 15:46:06 by thoberth          #+#    #+#             */
+/*   Updated: 2021/03/16 15:50:11 by thoberth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _CHECKER_
+#define _CHECKER_
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+
+
+#endif
